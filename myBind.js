e7a623e132311f07c265dc6003a14174a11df695
chenqixian <chenqixian@buaa.edu.cn>
2019-10-18 17:39:08 +0800

    my Bind

---
+++ myBind.js
@@ -0,0 +1,10 @@
+/**
+ * 封装函数 f，使 f 的 this 指向指定的对象
+ * @param {Function} f 
+ * @param {Object} oTarget 
+ */
+function bindThis(f, oTarget) {
+    return function() {
+        return f.apply(oTarget, [].slice.call(arguments));
+    }
+}
