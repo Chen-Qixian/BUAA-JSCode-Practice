3315c80f2b7b7c0b0b656f903677a48f086e0b2e
chenqixian <chenqixian@buaa.edu.cn>
2019-10-18 17:57:21 +0800

    unique

---
+++ unique.js
@@ -0,0 +1,27 @@
+// 无法去重{}
+Array.prototype.uniq1 = function() {
+    return [... new Set(this)];
+}
+// 无法去重{}
+Array.prototype.uniq2 = function() {
+    return Array.from(new Set(this));
+}
+// 无法去重NaN
+Array.prototype.uniq3 = function() {
+    let arr = this;
+    let a = [];
+    for(let i = 0, len = arr.length ; i < len ; i ++) {
+        if(a.indexOf(arr[i]) == -1) {
+            a.push(arr[i]);
+        }
+    }
+    return a;
+}
+// 优 能去重NaN,{}
+Array.prototype.uniq3 = function() {
+    let arr = this;
+    let obj = {};
+    return arr.filter(function(item) {
+        return obj.hasOwnProperty(typeof item + item) ? false : (obj[typeof item + item] = true);
+    })
+}
