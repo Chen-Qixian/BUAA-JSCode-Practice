625e52622478cf4270de57814b986da335f5e499
chenqixian <chenqixian@buaa.edu.cn>
2019-10-21 17:05:33 +0800

    formate Date

--- formatDate.js
+++ formatDate.js
@@ -1,3 +1,25 @@
+/**
+ *  按所给的时间格式输出指定的时间
+    格式说明
+    对于 2014.09.05 13:14:20
+    yyyy: 年份，2014
+    yy: 年份，14
+    MM: 月份，补满两位，09
+    M: 月份, 9
+    dd: 日期，补满两位，05
+    d: 日期, 5
+    HH: 24制小时，补满两位，13
+    H: 24制小时，13
+    hh: 12制小时，补满两位，01
+    h: 12制小时，1
+    mm: 分钟，补满两位，14
+    m: 分钟，14
+    ss: 秒，补满两位，20
+    s: 秒，20
+    w: 星期，为 ['日', '一', '二', '三', '四', '五', '六'] 中的某一个，本 demo 结果为 五
+ * @param {Object} oDate 
+ * @param {String} sFormat 
+ */
 function formatDate(oDate, sFormat) {
     let y = oDate.getFullYear(),
         m = oDate.getMonth() + 1,
