31ab909c05ac43be9b1bb90b2f250b19069ace48
chenqixian <chenqixian@buaa.edu.cn>
2019-11-08 10:36:41 +0800

    my bind version 2

--- myBind.js
+++ myBind.js
@@ -1,10 +1,27 @@
-/**
- * 封装函数 f，使 f 的 this 指向指定的对象
- * @param {Function} f 
- * @param {Object} oTarget 
- */
-function bindThis(f, oTarget) {
+Function.prototype.myBind = function(context) {
+    let _this = this; // 获取该函数调用者
+    let args = [].slice.call(arguments, 1);
+
     return function() {
-        return f.apply(oTarget, [].slice.call(arguments));
+        // 柯里化
+        let bindArgs = [].slice.call(arguments);
+        return _this.apply(context, args.concat(bindArgs));
     }
 }
+
+let value = 2;
+
+let foo = {
+    value: 1
+}
+
+function bar(name, age) {
+    return {
+        name: name,
+        age: age,
+        value: this.value
+    }
+}
+
+let bindFoo = bar.myBind(foo, 'hello');
+bindFoo(20);
